0b786e1d7d3a3c849b7f07570026dd27c6c39a3a
joaobug <joaoenfrance@gmail.com>
2024-09-16 11:15:53 -0300

    Initial Commit

---
+++ CarRentalAPP/CarRentalAPPApp.swift
@@ -0,0 +1,17 @@
+//
+//  CarRentalAPPApp.swift
+//  CarRentalAPP
+//
+//  Created by Joao Leal on 9/16/24.
+//
+
+import SwiftUI
+
+@main
+struct CarRentalAPPApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
